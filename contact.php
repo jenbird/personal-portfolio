3a8170367204d8860e90553f0a1fd10359ccd131
jennifersmithuk <jennifersmithuk@gmail.com>
2018-11-25 21:56:42 +0000

    fix: Contact form displays thank you/errors

--- contact.php
+++ contact.php
@@ -1,6 +1,7 @@
 <?php
-// require ReCaptcha class
-require('recaptcha-master/src/autoload.php');
+/*
+ *  CONFIGURE EVERYTHING HERE
+ */
 
 // an email address that will be in the From field of the email.
 $from = 'Jen Smith Web Developer <hello@jensmith.info>';
@@ -9,11 +10,11 @@ $from = 'Jen Smith Web Developer <hello@jensmith.info>';
 $sendTo = 'Jen Smith Web Developer <hello@jensmith.info>';
 
 // subject of the email
-$subject = 'New message from website form';
+$subject = 'New message from contact form';
 
 // form field names and their translations.
 // array variable name => Text to appear in the email
-$fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message');
+$fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message'); 
 
 // message that will be displayed when everything is OK :)
 $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
@@ -21,69 +22,54 @@ $okMessage = 'Contact form successfully submitted. Thank you, I will get back to
 // If something goes wrong, we will display this message.
 $errorMessage = 'There was an error while submitting the form. Please try again later';
 
-// ReCaptch Secret
-$recaptchaSecret = 'SECRET_KEY';
-
-// let's do the sending
+/*
+ *  LET'S DO THE SENDING
+ */
 
 // if you are not debugging and don't need error reporting, turn this off by error_reporting(0);
 error_reporting(E_ALL & ~E_NOTICE);
 
-try {
-    if (!empty($_POST)) {
+try
+{
 
-        // validate the ReCaptcha, if something is wrong, we throw an Exception,
-        // i.e. code stops executing and goes to catch() block
-        
-        if (!isset($_POST['g-recaptcha-response'])) {
-            throw new \Exception('ReCaptcha is not set.');
+    if(count($_POST) == 0) throw new \Exception('Form is empty');
+            
+    $emailText = "You have a new message from your contact form\n=============================\n";
+
+    foreach ($_POST as $key => $value) {
+        // If the field exists in the $fields array, include it in the email 
+        if (isset($fields[$key])) {
+            $emailText .= "$fields[$key]: $value\n";
         }
-
-        // do not forget to enter your secret key from https://www.google.com/recaptcha/admin
-        
-        $recaptcha = new \ReCaptcha\ReCaptcha($recaptchaSecret, new \ReCaptcha\RequestMethod\CurlPost());
-        
-        // we validate the ReCaptcha field together with the user's IP address
-        
-        $response = $recaptcha->verify($_POST['g-recaptcha-response'], $_SERVER['REMOTE_ADDR']);
-
-        if (!$response->isSuccess()) {
-            throw new \Exception('ReCaptcha was not validated.');
-        }
-        
-        // everything went well, we can compose the message, as usually
-        
-        $emailText = "You have a new message from your contact form\n=============================\n";
-
-        foreach ($_POST as $key => $value) {
-            // If the field exists in the $fields array, include it in the email
-            if (isset($fields[$key])) {
-                $emailText .= "$fields[$key]: $value\n";
-            }
-        }
-    
-        // All the neccessary headers for the email.
-        $headers = array('Content-Type: text/plain; charset="UTF-8";',
-            'From: ' . $from,
-            'Reply-To: ' . $from,
-            'Return-Path: ' . $from,
-        );
-        
-        // Send email
-        mail($sendTo, $subject, $emailText, implode("\n", $headers));
-
-        $responseArray = array('type' => 'success', 'message' => $okMessage);
     }
-} catch (\Exception $e) {
-    $responseArray = array('type' => 'danger', 'message' => $e->getMessage());
+
+    // All the neccessary headers for the email.
+    $headers = array('Content-Type: text/plain; charset="UTF-8";',
+        'From: ' . $from,
+        'Reply-To: ' . $from,
+        'Return-Path: ' . $from,
+    );
+    
+    // Send email
+    mail($sendTo, $subject, $emailText, implode("\n", $headers));
+
+    $responseArray = array('type' => 'success', 'message' => $okMessage);
+}
+catch (\Exception $e)
+{
+    $responseArray = array('type' => 'danger', 'message' => $errorMessage);
 }
 
+
+// if requested by AJAX request return JSON response
 if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
     $encoded = json_encode($responseArray);
 
     header('Content-Type: application/json');
 
     echo $encoded;
-} else {
+}
+// else just display the message
+else {
     echo $responseArray['message'];
 }
